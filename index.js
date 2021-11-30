3a837b7f16c4212cc9a676dcd9d873c872b20f8b
DominiqueFlorence <ddaniell1987@gmail.com>
2021-11-30 18:16:17 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(funActivity = 'roller-skate') { 
+    return `This Saturday, I want to ${funActivity}!`
+}
+saturdayFun();
+
+function mondayWork(workActivity= 'go to the office') {
+    return `This Monday, I will ${workActivity}.`
+}
+mondayWork();
+
+wrapAdjective('soo beautiful')('to me, cant you see')
+function wrapAdjective(sweetSong= '*') {
+    return function(adjective= 'special') {
+        return `You are ${sweetSong}${adjective}${sweetSong}!`
+    }
+}
